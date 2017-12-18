9021e7296a7321d8b12742d83972720ab884344f
apogre <mail.anishpradhan@gmail.com>
2017-12-18 00:10:12 -0700

    sample data

--- src/test/java/asu/edu/neg_rule_miner/ClientTest.java
+++ src/test/java/asu/edu/neg_rule_miner/ClientTest.java
@@ -1,11 +1,16 @@
 package asu.edu.neg_rule_miner;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.jena.ext.com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +24,39 @@ public class ClientTest
 {
   private final DynamicPruningRuleDiscovery rudik = new DynamicPruningRuleDiscovery();
   private final static Logger LOGGER = LoggerFactory.getLogger(ClientTest.class.getName());
+  
+  
+  protected Set<Pair<String,String>> getExamples(String csvFile) {
+      BufferedReader br = null;
+      String line = "";
+      String cvsSplitBy = ",";
+      final Set<Pair<String,String>> examples = Sets.newHashSet();
+      
+      try {
+          br = new BufferedReader(new FileReader(csvFile));
+          while ((line = br.readLine()) != null) {
+              String[] csvExample = line.split(cvsSplitBy);
+              System.out.println(csvExample[0] +" , " + csvExample[2]);
+              final Pair<String,String> example = Pair.of("http://dbpedia.org/resource/"+csvExample[0], "http://dbpedia.org/resource/"+csvExample[2]);
+  	    		examples.add(example);
+          }
+
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      } finally {
+          if (br != null) {
+              try {
+                  br.close();
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+          }
+      }
+	  return examples;
+
+  }
   
   protected List<HornRule> executeRudikNegativeRules(final Set<String> relationNames, final String subjectType, final String objectType){
     final Instant startTime = Instant.now();
@@ -47,10 +85,16 @@ public class ClientTest
     final Instant startTime = Instant.now();
     
     //get positive and negative examples
-    final Set<Pair<String,String>> negativeExamples = rudik.
-        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
-    final Set<Pair<String,String>> positiveExamples = rudik.
-        generatePositiveExamples(relationNames, subjectType, objectType);
+//    final Set<Pair<String,String>> negativeExamples = rudik.
+//        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
+//    final Set<Pair<String,String>> positiveExamples = rudik.
+//        generatePositiveExamples(relationNames, subjectType, objectType);
+    
+    String csvFile_positive = "/home/anish/fall17/rudik_new/Rudik/keyPerson_positive.csv";
+    String csvFile_negative = "/home/anish/fall17/rudik_new/Rudik/keyPerson_negative.csv";
+    final Set<Pair<String,String>> negativeExamples = getExamples(csvFile_negative);
+    final Set<Pair<String,String>> positiveExamples = getExamples(csvFile_positive);
+
 
     //compute outputs
     final Map<HornRule,Double> outputRules = rudik.
