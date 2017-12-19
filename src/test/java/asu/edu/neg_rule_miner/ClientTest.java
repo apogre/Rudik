4608c3dfe689453b32c0bcfda5be8516b7dbe53b
anishpradhan <mail.anishpradhan@gmail.com>
2017-12-18 17:08:20 -0700

    custom gp vp

--- src/test/java/asu/edu/neg_rule_miner/ClientTest.java
+++ src/test/java/asu/edu/neg_rule_miner/ClientTest.java
@@ -1,8 +1,10 @@
 package asu.edu.neg_rule_miner;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
@@ -16,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 import asu.edu.neg_rule_miner.model.horn_rule.HornRule;
 import asu.edu.neg_rule_miner.rule_generator.DynamicPruningRuleDiscovery;
+import au.com.bytecode.opencsv.CSVReader;
 
 /**
  * Unit test for simple App.
@@ -24,36 +27,35 @@ public class ClientTest
 {
   private final DynamicPruningRuleDiscovery rudik = new DynamicPruningRuleDiscovery();
   private final static Logger LOGGER = LoggerFactory.getLogger(ClientTest.class.getName());
+  private static final String export_path_all = "/home/apradhan/proj/fact_checking/LPmln/company_keyPerson/controlled/rudik_rules_all1000/";
+  private static final String export_path = "/home/apradhan/proj/fact_checking/LPmln/company_keyPerson/controlled/rudik_rules_compact/";
+  private static final String FILENAME_NEG_COMPACT = export_path+"keyPerson_neg.txt";
+  private static final String FILENAME_POS_COMPACT = export_path+"keyPerson_pos.txt";
+  private static final String FILENAME_NEG_ALL = export_path_all+"keyPerson_neg_all.txt";
+  private static final String FILENAME_POS_ALL = export_path_all+"keyPerson_pos_all.txt";
   
+  
+  
+  BufferedWriter bw = null;
+  FileWriter fw = null;
   
   protected Set<Pair<String,String>> getExamples(String csvFile) {
-      BufferedReader br = null;
-      String line = "";
-      String cvsSplitBy = ",";
       final Set<Pair<String,String>> examples = Sets.newHashSet();
+      CSVReader reader = null;
       
       try {
-          br = new BufferedReader(new FileReader(csvFile));
-          while ((line = br.readLine()) != null) {
-              String[] csvExample = line.split(cvsSplitBy);
-              System.out.println(csvExample[0] +" , " + csvExample[2]);
-              final Pair<String,String> example = Pair.of("http://dbpedia.org/resource/"+csvExample[0], "http://dbpedia.org/resource/"+csvExample[2]);
+    	  reader = new CSVReader(new FileReader(csvFile));
+          String[] line;
+          
+          while ((line = reader.readNext()) != null) {
+              System.out.println(line[0] +" , " + line[2]);
+              final Pair<String,String> example = Pair.of("http://dbpedia.org/resource/"+line[0], "http://dbpedia.org/resource/"+line[2]);
   	    		examples.add(example);
           }
 
-      } catch (FileNotFoundException e) {
-          e.printStackTrace();
       } catch (IOException e) {
           e.printStackTrace();
-      } finally {
-          if (br != null) {
-              try {
-                  br.close();
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-          }
-      }
+      } 
 	  return examples;
 
   }
@@ -62,10 +64,15 @@ public class ClientTest
     final Instant startTime = Instant.now();
     
     //get positive and negative examples
-    final Set<Pair<String,String>> negativeExamples = rudik.
-        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
-    final Set<Pair<String,String>> positiveExamples = rudik.
-        generatePositiveExamples(relationNames, subjectType, objectType);
+//    final Set<Pair<String,String>> negativeExamples = rudik.
+//        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
+//    final Set<Pair<String,String>> positiveExamples = rudik.
+//        generatePositiveExamples(relationNames, subjectType, objectType);
+    
+    String csvFile_positive = "keyPerson_pos.csv";
+    String csvFile_negative = "keyPerson_neg.csv";
+    final Set<Pair<String,String>> negativeExamples = getExamples(csvFile_negative);
+    final Set<Pair<String,String>> positiveExamples = getExamples(csvFile_positive);
 
     //compute outputs
     final List<HornRule> outputRules = rudik.
@@ -77,21 +84,74 @@ public class ClientTest
     LOGGER.info("----------------------------Final output rules----------------------------");
     for(final HornRule oneRule:outputRules){
       LOGGER.info("{}",oneRule);
+      try{
+    	  fw = new FileWriter(FILENAME_NEG_COMPACT, true);
+    	  bw = new BufferedWriter(fw);
+    	  bw.write(oneRule.toString()+"\n");
+      }catch (IOException e){
+    	  e.printStackTrace();
+      }finally{
+    	  if (bw != null) try {
+    		    bw.close();
+    		 } catch (IOException ioe2) {
+    		    // just ignore it
+    		 }
+      }
     }
     return outputRules;
   }
   
+  protected List<HornRule> executeRudikPositiveRules(final Set<String> relationNames, final String subjectType, final String objectType){
+	    final Instant startTime = Instant.now();
+	    
+	    //get positive and negative examples
+//	    final Set<Pair<String,String>> negativeExamples = rudik.
+//	        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
+//	    final Set<Pair<String,String>> positiveExamples = rudik.
+//	        generatePositiveExamples(relationNames, subjectType, objectType);
+//	    
+	    String csvFile_positive = "keyPerson_pos.csv";
+	    String csvFile_negative = "keyPerson_neg.csv";
+	    final Set<Pair<String,String>> negativeExamples = getExamples(csvFile_negative);
+	    final Set<Pair<String,String>> positiveExamples = getExamples(csvFile_positive);
+	    
+	    final List<HornRule> outputRules = rudik.discoverPositiveHornRules(negativeExamples, positiveExamples, relationNames, subjectType, objectType);
+	    //compute outputs
+	    	    
+	    final Instant endTime = Instant.now();
+	    LOGGER.info("----------------------------COMPUTATION ENDED----------------------------");
+	    LOGGER.info("Final computation time: {} seconds.",(endTime.toEpochMilli()-startTime.toEpochMilli())/1000.);
+	    LOGGER.info("----------------------------Final output rules----------------------------");
+	    for(final HornRule oneRule:outputRules){
+	      LOGGER.info("{}",oneRule);
+	      try{
+	    	  fw = new FileWriter(FILENAME_POS_COMPACT, true);
+	    	  bw = new BufferedWriter(fw);
+	    	  bw.write(oneRule.toString()+"\n");
+	      }catch (IOException e){
+	    	  e.printStackTrace();
+	      }finally{
+	    	  if (bw != null) try {
+	    		    bw.close();
+	    		 } catch (IOException ioe2) {
+	    		    // just ignore it
+	    		 }
+	      }
+	    }
+	    return outputRules;
+	  }
+  
   protected Map<HornRule,Double> executeRudikAllNegativeRules(final Set<String> relationNames, final String subjectType, final String objectType, final int maxRulesNumber){
     final Instant startTime = Instant.now();
     
-    //get positive and negative examples
+//    get positive and negative examples
 //    final Set<Pair<String,String>> negativeExamples = rudik.
 //        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
 //    final Set<Pair<String,String>> positiveExamples = rudik.
 //        generatePositiveExamples(relationNames, subjectType, objectType);
     
-    String csvFile_positive = "/home/anish/fall17/rudik_new/Rudik/keyPerson_positive.csv";
-    String csvFile_negative = "/home/anish/fall17/rudik_new/Rudik/keyPerson_negative.csv";
+    String csvFile_positive = "keyPerson_pos.csv";
+    String csvFile_negative = "keyPerson_neg.csv";
     final Set<Pair<String,String>> negativeExamples = getExamples(csvFile_negative);
     final Set<Pair<String,String>> positiveExamples = getExamples(csvFile_positive);
 
@@ -106,7 +166,59 @@ public class ClientTest
     LOGGER.info("----------------------------Final output rules----------------------------");
     for(final HornRule oneRule:outputRules.keySet()){
       LOGGER.info("Rule:{}\tScore:{}",oneRule,outputRules.get(oneRule));
+      try{
+    	  fw = new FileWriter(FILENAME_NEG_ALL, true);
+    	  bw = new BufferedWriter(fw);
+    	  bw.write(oneRule.toString()+","+String.format("%.5f",outputRules.get(oneRule))+"\n");
+      }catch (IOException e){
+    	  e.printStackTrace();
+      }finally{
+    	  if (bw != null) try {
+    		    bw.close();
+    		 } catch (IOException ioe2) {
+    		    // just ignore it
+    		 }
+      }
     }
     return outputRules;
   }
+  
+  protected Map<HornRule,Double> executeRudikAllPositiveRules(final Set<String> relationNames, final String subjectType, final String objectType, final int maxRulesNumber){
+	    final Instant startTime = Instant.now();   
+	    //get positive and negative examples
+//	    final Set<Pair<String,String>> negativeExamples = rudik.
+//	        generateNegativeExamples(relationNames, subjectType, objectType, false, false);
+//	    final Set<Pair<String,String>> positiveExamples = rudik.
+//	        generatePositiveExamples(relationNames, subjectType, objectType);
+	    
+	    String csvFile_positive = "keyPerson_pos.csv";
+	    String csvFile_negative = "keyPerson_neg.csv";
+	    final Set<Pair<String,String>> negativeExamples = getExamples(csvFile_negative);
+	    final Set<Pair<String,String>> positiveExamples = getExamples(csvFile_positive);
+	    
+	    final Map<HornRule, Double> outputRules = rudik.discoverAllPositiveHornRules(negativeExamples, positiveExamples, relationNames, subjectType, objectType, maxRulesNumber); 
+	    
+	    final Instant endTime = Instant.now();
+	    LOGGER.info("----------------------------COMPUTATION ENDED----------------------------");
+	    LOGGER.info("Final computation time: {} seconds.",(endTime.toEpochMilli()-startTime.toEpochMilli())/1000.);
+	    LOGGER.info("----------------------------Final output rules----------------------------");
+	    for(final HornRule oneRule:outputRules.keySet()){
+	      LOGGER.info("Rule:{}\tScore:{}",oneRule,outputRules.get(oneRule));
+	      try{
+	    	  fw = new FileWriter(FILENAME_POS_ALL, true);
+	    	  bw = new BufferedWriter(fw);
+	    	  bw.write(oneRule.toString()+","+String.format("%.5f",outputRules.get(oneRule))+"\n");
+	      }catch (IOException e){
+	    	  e.printStackTrace();
+	      }finally{
+	    	  if (bw != null) try {
+	    		    bw.close();
+	    		 } catch (IOException ioe2) {
+	    		    // just ignore it
+	    		 }
+	      }
+	      }
+	    return outputRules;
+	  }
+  
 }
--- src/test/java/asu/edu/neg_rule_miner/dbpedia/DBPediaClient.java
+++ src/test/java/asu/edu/neg_rule_miner/dbpedia/DBPediaClient.java
@@ -22,25 +22,46 @@ public class DBPediaClient extends ClientTest{
   }
   
   
-  @Test
-  public void testSpouseNegative() {
-    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/spouse");
-    final String typeSubject = "http://dbpedia.org/ontology/Person";
-    final String typeObject = "http://dbpedia.org/ontology/Person";
-    Assert.assertNotNull(super.executeRudikNegativeRules(relations, typeSubject, typeObject)); 
-  }
+//  @Test
+//  public void testSpouseNegative() {
+//    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/keyPerson");
+//    final String typeSubject = "http://dbpedia.org/ontology/Company";
+//    final String typeObject = "http://dbpedia.org/ontology/Person";
+//    Assert.assertNotNull(super.executeRudikNegativeRules(relations, typeSubject, typeObject)); 
+//  }
+  
+//  @Test
+//  public void testSpousePositive() {
+//    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/keyPerson");
+//    final String typeSubject = "http://dbpedia.org/ontology/Company";
+//    final String typeObject = "http://dbpedia.org/ontology/Person";
+//    Assert.assertNotNull(super.executeRudikPositiveRules(relations, typeSubject, typeObject)); 
+//  }
   
   @Test
   public void testSpouseAllNegative() {
     //target relations to be discovered
-    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/spouse");
+    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/keyPerson");
     //type of the subject according to the ontology
-    final String typeSubject = "http://dbpedia.org/ontology/Person";
+    final String typeSubject = "http://dbpedia.org/ontology/Company";
     //type of the object according to the ontology
     final String typeObject = "http://dbpedia.org/ontology/Person";
     //max number of rules to have in output. If set to a negative number, it will return all the ruls
-    final int maxRulesNumber = 10;
+    final int maxRulesNumber = 1000;
     Assert.assertNotNull(super.executeRudikAllNegativeRules(relations, typeSubject, typeObject,maxRulesNumber)); 
+  }
+  
+  @Test
+  public void testSpouseAllPositive() {
+    //target relations to be discovered
+    final Set<String> relations = Sets.newHashSet("http://dbpedia.org/ontology/keyPerson");
+    //type of the subject according to the ontology
+    final String typeSubject = "http://dbpedia.org/ontology/Company";
+    //type of the object according to the ontology
+    final String typeObject = "http://dbpedia.org/ontology/Person";
+    //max number of rules to have in output. If set to a negative number, it will return all the ruls
+    final int maxRulesNumber = 1000;
+    Assert.assertNotNull(super.executeRudikAllPositiveRules(relations, typeSubject, typeObject,maxRulesNumber));
   }
 
 }
